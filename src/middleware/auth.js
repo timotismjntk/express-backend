c6f04411f7657b461f7de28848d83e8801598319
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-09-28 09:00:47 +0700

    creating middleware for authentication

---
+++ src/middleware/auth.js
@@ -0,0 +1,39 @@
+const qs = require('querystring')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const responseStandard = require('../helpers/response')
+
+module.exports = {
+    authUser: (req, res, next) => {
+        const {authorization} = req.headers
+        if(authorization && authorization.startsWith('Bearer ')){
+            const token = authorization.slice(7, authorization.length)
+            try{
+                const payload = jwt.verify(token, process.env.CUST_SECRET_KEY)
+                if(payload){
+                    console.log('this' +res)
+                    req.user = payload
+                    next()
+                }else {
+                    return responseStandard(res, 'Unauthorized', {}, 401, false)
+                }
+            }
+            catch(err){
+                return responseStandard(res, err.message, {}, 500, false)
+            }
+        }else {
+            return responseStandard(res, 'Forbidden Access', {}, 403, false )
+        }
+    },
+    authRole: (role) => {       // authentication to access admin page
+        return (req, res, next) => {
+            console.log(req)
+            if (req.user.role_id !== role) {
+                return responseStandard(res, 'Not Have Access', {}, 401, false)
+            }
+            next();
+        }
+    }
+}
+
