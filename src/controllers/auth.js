2c0a43e1dd351314cb6a1801e27f287a8beab19b
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-09-29 22:01:14 +0700

    update authentication controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -13,29 +13,29 @@ module.exports = {
             const data = db.query("SELECT * FROM `users` WHERE `email` ='"+email+"'", (err, result) => {
             // console.log(data)
             if(result.length) {
+                console.log(result[0].role_id)
                 var uid = result[0].id
                 var mail = result[0].email
                 var role = result[0].role_id
-                console.log(role)
+                console.log('ini' +role)
                 if(result[0].password){
                     bcrypt.compare(password, result[0].password, function(err, result) {
                         console.log('Ini ', password)
                         // console.log('condition ', result)
                         console.log('condition ', mail)
-
                         if(result) {
                             jwt.sign({role_id: role, email: mail, id: uid}, process.env.CUST_SECRET_KEY, {expiresIn: 1511}, (err, token)=>{
                                 return responseStandard(res, `Token ${token}`)
                             }) 
                         }
                         else {
-                            return responseStandard(res, 'Wrong email or password', {}, 400, false)
+                            return responseStandard(res, 'Wrong email ord password', {}, 400, false)
                         }
                         })
                 }
-            }else {
-                return responseStandard(res, 'Wrong email or password', {}, 400, false)
-            }
+                }else {
+                    return responseStandard(res, 'Wrong email or password', {}, 400, false)
+                }
             })
         } else {
             return responseStandard(res, 'Please fill the form', {}, 400, false)
