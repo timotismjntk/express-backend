16292ab09cc89c3b1ac03339d7a5626681110901
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-09-30 23:25:29 +0700

    add route for signup

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,8 @@
 const route = require('express').Router();
-const {loginController} = require('../controllers/auth')
+const {loginController, signUpController, signUpSellerController } = require('../controllers/auth')
 
 route.post('/login', loginController)
+route.post('/signup', signUpController)
+route.post('/seller/signup', signUpSellerController)
+
 module.exports = route
