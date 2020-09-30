b6e6ad5f919cf39aaeb198072fc83c861640a7fa
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-09-30 23:26:51 +0700

    update routes for manage user

--- src/routes/manageProduct.js
+++ src/routes/manageProduct.js
@@ -9,21 +9,21 @@ const {
   deleteProduct
 } = require('../controllers/productController')
 
-// const authMiddleware = require('../middleware/auth')
-// authMiddleware.authRole()  adalah cara untuk membatasi jika bukan admin maka tidak akan bisa mengakses route manages role
+const authMiddleware = require('../middleware/auth')
+// authMiddleware.authRole()  //adalah cara untuk membatasi jika bukan admin maka tidak akan bisa mengakses route manages role
 
 const router = Router()
 
-// "id": 1,
-// "name": "Super Admin",
-// "description": "Responsible to manage roles",
+// "id": 2,
+// "name": "Seller",
+// "description": "Responsible to product",
 
 router.get('/', read)
 router.get('/new/', getNewProduct)
 router.get('/:id', getProductId)
-router.post('/', create)
-router.put('/:id', updateProduct)
-router.patch('/:id', updateProductPartial)
-router.delete('/:id', deleteProduct)
+router.post('/', authMiddleware.authRole(2), create)
+router.put('/:id', authMiddleware.authRole(2), updateProduct)
+router.patch('/:id', authMiddleware.authRole(2), updateProductPartial)
+router.delete('/:id', authMiddleware.authRole(2), deleteProduct)
 
 module.exports = router
