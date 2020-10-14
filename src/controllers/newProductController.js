4a27e71a084dd0bf71de3cbf8eceb7ba27a7875e
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-10-15 01:23:46 +0700

    update newest product controller

--- src/controllers/newProductController.js
+++ src/controllers/newProductController.js
@@ -10,7 +10,7 @@ module.exports = {
     let { search, orderBy } = req.query
     const page = paging(req, count)
     let { offset=0, pageInfo } = page
-    const { limitData: limit=5 } = pageInfo
+    const { limitData: limit } = pageInfo
     if (typeof search === 'object') {
         searchKey = Object.keys(search)[0]
         searchValue = Object.values(search)[0]
@@ -22,11 +22,12 @@ module.exports = {
     orderByKey = Object.keys(orderBy)[0]
     orderByValue = Object.values(orderBy)[0]
     } else {
-    orderByKey = 'id'
-    orderByValue = orderBy || 'ASC'
+    orderByKey = 'product.created_at'
+    orderByValue = orderBy || 'DESC'
     }
+    console.log(count)
     const results = await NewProductModel.readNewProduct([searchKey, searchValue, orderByKey, orderByValue, limit, offset])
-    console.log(results)
+    // console.log(results)
     return responseStandard(res, 'List of Product', {results, pageInfo})
   }
 }
