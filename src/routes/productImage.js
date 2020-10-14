55378230d5fb164b98ec00eda337497417a0d058
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-10-15 00:29:29 +0700

    update route for image product

--- src/routes/productImage.js
+++ src/routes/productImage.js
@@ -24,8 +24,8 @@ const router = Router()
 router.get('/', read)
 router.get('/:id', getProductImageId)
 router.post('/', authMiddleware.authUser, authMiddleware.authRole(2), create)
-router.put('/:id', uploadHelper, authMiddleware.authUser, authMiddleware.authRole(2), updateProductImage)
-router.patch('/:id', uploadHelper, authMiddleware.authRole(2), updateProductImagePartial)
-router.delete('/:id', authMiddleware.authRole(2), deleteProductImage)
+router.put('/:id', authMiddleware.authUser, authMiddleware.authRole(2), updateProductImage)
+// router.patch('/:id', authMiddleware.authUser, authMiddleware.authRole(2), updateProductImagePartial)
+router.delete('/:id', deleteProductImage)
 
 module.exports = router
